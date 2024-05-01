be94beaa52d22224ea98d9f409197aa05f91b3d7
Prabhat <prabhat@Prabhats-Macbook.local>
2024-05-01 14:36:49 +0530

    Task controller and cron

---
+++ server/config/mailer.ts
@@ -0,0 +1,12 @@
+import nodemailer from "nodemailer";
+
+export const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.MAIL_USERNAME,
+    pass: process.env.MAIL_PASSWORD,
+  },
+});
--- server/cron/reminderCron.ts
+++ server/cron/reminderCron.ts
@@ -1,13 +1,59 @@
 import cron, { Patterns } from "@elysiajs/cron";
+import { transporter } from "../config/mailer";
 import ReminderModel from "../models/reminder.model";
 import UserModel from "../models/User.model";
 import Logger from "../utils/Logger";
 
 const logger = new Logger("Reminder Cron");
 
-const sendReminderNotification = (email: string, task: string) => {
+const sendReminderNotification = async (email: string, task: string) => {
   try {
-    //1. Send out emails
+    const sent = await transporter.sendMail({
+      from: "noreply@mail",
+      to: email,
+      subject: `Task Reminder : ${task}`,
+      text: `
+      <!DOCTYPE html>
+        <html lang="en">
+        <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Reminder: ${task}</title>
+        <style>
+          /* Styles for better email formatting */
+          body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+          }
+          .container {
+            max-width: 600px;
+            margin: 20px auto;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0,0,0,.1);
+          }
+          h1, p {
+            margin: 0 0 20px 0;
+          }
+        </style>
+        </head>
+        <body>
+          <div class="container">
+            <h1>Reminder: ${task}</h1>
+            <p>Hello,</p>
+            <p>This is a friendly reminder about ${task}. Please don't forget!</p>
+            <p>Thank you.</p>
+          </div>
+        </body>
+        </html>
+
+      `,
+    });
+    return;
   } catch (err) {
     logger.error(err);
   }
